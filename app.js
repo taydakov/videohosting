4f0d56666b312ba611e784150c02687c7b188d8f
Lev Taydakov <lev.taydakov@gmail.com>
2014-10-28 10:07:52 -0700

    simple upload form and logic implemented

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var multipart = require('connect-multiparty');
+var multiparty = require('multiparty');
+var util = require('util');
 var path = require('path');
 
 var app = express();
@@ -12,18 +13,28 @@ app.set('view engine', 'ejs');
 /* middleware */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/upload', multipart({
-	uploadDir: "./tmp"
-}), function (req, res) {
-	return res.json(200, req.files);
-});
 
 /* routes */
 app.get('/', require('./routes/index'));
+app.post('/upload', function(req, res) {
+	var form = new multiparty.Form({
+		autoFiles: true,
+		uploadDir: path.join(__dirname, 'tmp')
+	});
+
+	form.parse(req, function(err, fields, files) {
+		res.writeHead(200, {'content-type': 'text/plain'});
+		res.write('received upload:\n\n');
+		res.end(util.inspect({fields: fields, files: files}));
+		console.log(files);
+	});
+
+	return;
+});
 
 /* catch error */
 app.use(function(req, res, next) {
-	res.end('Troubles!');
+	res.send(500, 'Internal Server Error');
 });
 
 /* web server */
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
 	res.render('index', {
-		par1: "Hello, world"
+		// parameters
 	});
 });
 
