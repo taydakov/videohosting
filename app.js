6b51147792513ee2c0e445394f33dc897dae5c0a
Lev Taydakov <lev.taydakov@gmail.com>
2014-10-28 09:04:39 -0700

    implemented upload route

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multipart = require('connect-multiparty');
+
 var app = express();
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/upload', multipart({
+	uploadDir: "./tmp"
+}), function (req, res) {
+	return res.json(200, req.files);
+});
 
 app.get('/', function(req, res) {
 	res.send('Hello World!');
